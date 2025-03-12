a10075c228419071e9f238b1467b9e549163824e
mikealiaga <mikealiaga97@gmail.com>
2025-03-12 01:06:17 -0600

    implemented tailwind, flowbite created file structure and example of a sidebar and example login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Sidebar from './components/sidebar'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
 
 function App() {
-  
-
   return (
-    <>
+    <Router>
       <div>
-       <Sidebar />
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </div>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/dashboard.tsx
@@ -0,0 +1,16 @@
+import Sidebar from '../components/sidebar'
+
+function Dashboard() {
+  
+
+    return (
+      <>
+        <div>
+            <Sidebar />
+        </div>
+      </>
+    )
+  }
+  
+  export default Dashboard;
+  
