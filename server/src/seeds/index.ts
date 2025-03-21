a2a83e648f51063aea5c1aa965987911dd528268
FabianSaldierna <fsaldierna@banxico.org.mx>
2025-03-20 19:08:59 -0600

    changes to DB

--- server/src/seeds/index.ts
+++ server/src/seeds/index.ts
@@ -73,13 +73,13 @@ connection.once('open', async () => {
 
     //seed the feeling_catalog
     const feeling_catalogs: any[] = [
-        { _id: "67d0cd2ecdd8c1addba64561", feeling_name: "anger", feeling: "1" },
+        { _id: "67d0cd2ecdd8c1addba64561", feeling_name: "angry", feeling: "1" },
         { _id: "67d0cd2ecdd8c1addba64562", feeling_name: "happy", feeling: "2" },
         { _id: "67d0cd2ecdd8c1addba64563", feeling_name: "sad", feeling: "3" },
         { _id: "67d0cd2ecdd8c1addba64564", feeling_name: "fear", feeling: "4" },
-        { _id: "67d0cd2ecdd8c1addba64565", feeling_name: "disgust", feeling: "5" },
+        { _id: "67d0cd2ecdd8c1addba64565", feeling_name: "disgust", feeling: "5" }, //quitar
         { _id: "67d0cd2ecdd8c1addba64566", feeling_name: "anxiety", feeling: "6" },
-        { _id: "67d0cd2ecdd8c1addba64567", feeling_name: "grief", feeling: "7" },
+        { _id: "67d0cd2ecdd8c1addba64567", feeling_name: "grief", feeling: "7" }, //quitar
     ];
 
     await Feelings_Catalog.insertMany(feeling_catalogs);  
