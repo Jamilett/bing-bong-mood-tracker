8285c18d95c55a1e76eed4c7809fcb579970905b
AlessandroJuleo <alessandrolc8003@gmail.com>
2025-03-20 20:56:33 -0600

    responsive and colors modification

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -1,40 +1,42 @@
-import Sidebar from '../components/sidebar'
-import LatestMood from '../components/latestmood';
-import Sevendays from '../components/sevendays';
-import Month from '../components/month';
-import Alltime from '../components/alltime';
-import MoodCounter from '../components/moodcounter';
-import History from '../components/history';
+import Sidebar from "../components/sidebar";
+import LatestMood from "../components/latestmood";
+import Sevendays from "../components/sevendays";
+import Month from "../components/month";
+import Alltime from "../components/alltime";
+import MoodCounter from "../components/moodcounter";
+import History from "../components/history";
 
 function Dashboard() {
+  return (
+    <>
+      <main className="bg-purple-50 min-h-screen">
+        <div>
+          <Sidebar />
+        </div>
 
+        <section className="flex flex-col p-4 gap-4 sm:ml-64">
+          <h2 className="text-2xl font-bold text-gray-900">Dashboard</h2>
 
-    return (
-        <>
-        <main className='bg-purple-50 min-h-screen'>
-            <div>
-                <Sidebar />
-            </div>
-            <section className='flex flex-col p-4 gap-4'>
-                <div>
-                    <h2 className='sm:ml-64 flex text-2xl font-bold text-gray-900'>Dashboard</h2>
-                </div>
-                <div className='sm:ml-64 flex gap-4'>
-                    <Sevendays />
-                    <Month />
-                    <Alltime />
-                </div>
-                <div className="sm:ml-64 grid grid-cols-1 md:grid-cols-2 gap-6">
-                    <MoodCounter />
-                    <LatestMood />
-                </div>
-                <div className='sm:ml-64'>
-                    <History />
-                </div>
-            </section>
-        </main>
-        </>
-    )
+          {/* Secciones en una sola columna en móviles */}
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            <Sevendays />
+            <Month />
+            <Alltime />
+          </div>
+
+          {/* Secciones en móvil 1 por fila y 2 columnas en desktop */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <MoodCounter />
+            <LatestMood />
+          </div>
+
+          <div>
+            <History />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default Dashboard;
